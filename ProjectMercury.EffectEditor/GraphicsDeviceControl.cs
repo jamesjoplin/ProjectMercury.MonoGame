9d329d0c2165d26eb10e3b4dca65dffeefa563da
James <thejayjay@gmail.com>
2018-02-17 12:50:24 -0500

    #1 Adjust projects to point to monogame assemblies.

--- ProjectMercury.EffectEditor/GraphicsDeviceControl.cs
+++ ProjectMercury.EffectEditor/GraphicsDeviceControl.cs
@@ -160,7 +160,7 @@ namespace ProjectMercury.EffectEditor
                 Rectangle sourceRectangle = new Rectangle(0, 0, ClientSize.Width,
                                                                 ClientSize.Height);
 
-                GraphicsDevice.Present(sourceRectangle, null, this.Handle);
+                GraphicsDevice.Present();
             }
             catch
             {
